f04896e987476ebf56d305a9970e5bd8d08a9419
Emily <emily.rose.jaxson@gmail.com>
2013-06-14 17:14:00 -0600

    Castle Flythrough/.svn/all-wcprops

---
+++ demos/Castle Flythrough/modelsMedevil.py
@@ -0,0 +1,55 @@
+from Panda import*
+
+
+def terrain(**a):
+    return modelHandle(fileName = "terrain.egg",  **a)
+
+def smith(**a):
+    return modelHandle(fileName = "moop_temp.egg", **a)
+
+def hut1(**a):
+    return modelHandle(fileName = "hut1.egg", **a)
+
+def turret(**a):
+    return modelHandle(fileName = "turret.egg", **a)
+
+def wall(**a):
+    return modelHandle(fileName = "stonewalll.egg", **a)
+
+def gallows(**a):
+    return modelHandle(fileName = "gallows.egg", **a)
+
+def castle(**a):
+    return modelHandle(fileName = "castle.egg", **a)
+
+def hut2(**a):
+    return modelHandle(fileName = "samHut22.egg",  **a)
+
+def building(**a):
+    return modelHandle(fileName = "SAMS BUILDING.egg", **a)
+
+def gun(**a):
+    return modelHandle(fileName = "SAMS GUN.egg", **a)
+
+def tower(**a):
+    return modelHandle(fileName = "sams rook.egg", **a)
+
+def sword(**a):
+    return modelHandle(fileName = "SAMS SWORD.egg", **a)
+
+def axe(**a):
+    return modelHandle(fileName = "SAMS AXE.egg", **a)
+
+def catapult(**a):
+    return modelHandle(fileName = "SAMS CATAPULT.egg", **a)
+
+def mace(**a):
+    return modelHandle(fileName = "SAMS MACE.egg", **a)
+
+def spear(**a):
+    return modelHandle(fileName = "SAMS SPEAR.egg", **a)
+
+
+
+
+
