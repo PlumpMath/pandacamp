ddc7b3cda0b36256825bf1a96711227d1cdabc9e
Emily <emily.rose.jaxson@gmail.com>
2013-06-20 13:35:17 -0600

    fixed some bugs in file utils racetrack and the Castle Flythrough/.svn/all-wcprops

--- handouts/Handouts 2012/4-2 Drive Game/01-Racegame.py
+++ handouts/Handouts 2012/4-2 Drive Game/01-Racegame.py
@@ -18,16 +18,16 @@ soundmove = Sound("engine.wav", loopCount = 0)
 def driving(model, p0, h0, speed0):
     print "Driving"
 #    text(speed0)
-    a1 = hold(0, key("arrow_right",  -1) + keyUp("arrow_right",  0))
-    a2 = hold(0, key("arrow_left",  1) + keyUp("arrow_left",  0))
-    a3 = hold(0, key("arrow_up",  1) + keyUp("arrow_up",  0))
-    a4 = hold(0, key("arrow_down",  -1) + keyUp("arrow_down",  0))
+    a1 = hold(0, key("rightArrow",  -1) + keyUp("rightArrow",  0))
+    a2 = hold(0, key("leftArrow",  1) + keyUp("leftArrow",  0))
+    a3 = hold(0, key("upArrow",  1) + keyUp("upArrow",  0))
+    a4 = hold(0, key("downArrow",  -1) + keyUp("downArrow",  0))
     delta = a1 + a2
     dspeed = a3 + a4
- 
+
     decay = -1.95*model.angle   # Straighten wheels after turning
     model.angle = integral(delta + decay)   # Should be limited
-    
+
     # the force on the vehicle
     forward = (speed0 * 4) + engineForce * integral(dspeed)
     # velocity
@@ -42,67 +42,67 @@ def driving(model, p0, h0, speed0):
     speed1 = choose(speed < -.4, -.4, speed)
     # heading
     h = h0 + integral(speed1 * model.angle)
-    model.hpr = HPR(h,0,0)
-    model.velocity = P3C(speed1,h-pi/2,0)
+    model.hpr = hpr(h,0,0)
+    model.velocity = p3c(speed1,h-pi/2,0)
     model.position = p0 + integral(model.velocity)
     model.when1(abs(model.angle*abs(model.velocity)*abs(model.velocity)) > spinThresh, spinout)
     model.when1(track.inWall(model), wallburn)
     model.react1(key(" "), jump)
-    
+
 def driveReset(model, var):
     drive(model, var, resetSpeed = 1)
-    
+
 def spinout(model, var):
     print "SPINNIN'"
     p0 = now(model.position)
     hpr0 = now(model.hpr)
     v0 = now(model.velocity)
     model.position = p0 + integral(v0 * (1 - localTime / 3))
-    model.hpr = hpr0 + HPR(integral(20 * (1 - localTime / 3)),0,0)
+    model.hpr = hpr0 + hpr(integral(20 * (1 - localTime / 3)),0,0)
     soundwipeout.play()
     # drive away
     model.react1(localTimeIs(3), driveReset)
 
 def chp(hpr):
-    return HPR(getH(hpr), -getP(hpr), getR(hpr))
+    return hpr(getH(hpr), -getP(hpr), getR(hpr))
 
 def jump(model, var):
     print "Jumping"
     p0 = now(model.position)
     v0 = now(model.velocity)
     hpr0 = now(model.hpr)
-    
-    model.velocity = P3(getX(v0), getY(v0), 1.5) + integral(P3(0,0,-1))
+
+    model.velocity = p3(getX(v0), getY(v0), 1.5) + integral(p3(0,0,-1))
     model.position = p0 + integral(model.velocity)
-    
-    model.hpr = chp(P3toHPR(deriv(model.position, HPRtoP3(hpr0))))
-    
+
+    model.hpr = chp(p3tohpr(deriv(model.position, hprToP3(hpr0))))
+
     model.when1(getZ(model.position)<0, drive)
 
 def drive(model, var, resetSpeed = 0):
     p0 = now(model.position)
     hpr0 = now(model.hpr)
     v0 = 0 if resetSpeed == 1 else now(model.velocity)
-    driving(model, P3(getX(p0), getY(p0), 0), getH(hpr0), abs(v0))
+    driving(model, p3(getX(p0), getY(p0), 0), getH(hpr0), abs(v0))
 
 def burning(model, p0, hpr0):
     print "burning"
-    
+
     model.position = p0
     model.hpr = hpr0
-    s = fireish(position = p0, duration = 1)
+    fireish(position = p0, duration = 1)
 
     # reset the model
     model.react1(localTimeIs(1), respawn)
 
 def respawn(m, v):
-    driving(car, P3(4,4,0), pi/2, 0)
+    driving(car, p3(4,4,0), pi/2, 0)
 
 # burning reaction
 def burn(model, var):
     # preserve state
     p = now(model.position)
-    hpr = now(model.hpr)
+    #hpr = now(model.hpr)
     # burning!
     play("bad.mp3")
     burning(model, p, hpr)
@@ -129,7 +129,7 @@ def explosion(model, var):
 
 # item generation
 def generateObj(model, var):
-    track.placeObj(coin, size = .1, position = P3(randomRange(5,track.xmax-5),randomRange(5,track.ymax-5),sin(time * 4) / 64 + .05), score = 1, reaction = powerUp, duration = 30, sound = "good.mp3")
+    track.placeObj(coin, size = .1, position = p3(randomRange(5,track.xmax-5),randomRange(5,track.ymax-5),sin(time * 4) / 64 + .05), score = 1, reaction = powerUp, duration = 30, sound = "good.mp3")
 
 # Other diving reactions
 #def setSpeed(m, v):
@@ -144,7 +144,7 @@ a = alarm(step = 4)
 react(a, generateObj)
 
 
-driving(car, P3(4,4,0), pi/2, 0)
-#car.react(dist(car.position, P3(5,5,0))<.5, jump) #When the car hits the panda
+driving(car, p3(4,4,0), pi/2, 0)
+#car.react(dist(car.position, p3(5,5,0))<.5, jump) #When the car hits the panda
 
 start()
