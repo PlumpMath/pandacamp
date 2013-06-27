2afddc35651955047bf91617a6f2e00b88e307c7
ProfessorJohnPeterson <jpeterson@western.edu>
2013-06-27 07:51:40 -0600

    First cut at flying

--- engine/Fly.py
+++ engine/Fly.py
@@ -1,24 +1,38 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+
+
 
 
 from Panda import *
 
 
-m = boeing707()
 
-camera.position = p3(0,-50,0)
+m = hangGlider()
+grassScene(size=10)
+#camera.position = p3(0,-50,0)
+camera.flatrod(m)
 
-friction = -0.1
+friction = -1
 climbK = 1
-rv = getX(mouse)
-pv = getY(mouse)
+rv = -getX(mouse)*.1
+pv = (getY(mouse)+1)/2 * .6 + .7
 gravity = p3(0,0,-1)
-thrust = slider(max = 5)
+#thrust = slider(max = 5)
+thrust = 1
 m.position = p3(15, 0, 0) + integral(m.velocity)
-m.velocity = p3(-5,0,0) + integral(m.accel)
-roll = 0 + integral(rv*abs(m.velocity))
+m.velocity = p3(-1,0,0) + integral(m.accel)
+roll = integral(rv*abs(m.velocity))
 hpr1 = p3ToHpr(m.velocity)
 m.hpr = hpr(getH(hpr1), getP(hpr1), roll)
-m.accel = gravity + abs(m.velocity) * friction * norm(m.velocity) + getUp(m.hpr)*climbK*abs(m.velocity)
+fwd = (thrust + abs(m.velocity) * friction) * norm(m.velocity)
+lift = getUp(m.hpr)*climbK*abs(m.velocity)*pv
+panda(position = m.position + getUp(m.hpr), size = .1)
+text(format("Position: %s", m.position))
+text(format("Velocity: %s", m.velocity))
+text(format("Up: %s",  getUp(m.hpr)))
+text(format("HPR: %s", m.hpr))
+text(format("Gravity: %s", gravity))
+text(format("Thrust: %s", fwd))
+text(format("Lift: %s", lift))
+
+m.accel = gravity + fwd + lift
 start()
